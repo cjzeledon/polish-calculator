f3e21e9096ec18ec122b6110cff04870922524f1
Claudia Zeledon <cjzeledon@gmail.com>
2017-09-14 19:04:18 -0400

    Completed by Claudia

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -7,20 +7,27 @@ package com.theironyard.charlotte;
  */
 public class Main {
     public static void main(String[] args) {
+        // the keyword called "new" creates new instances
         ReversePolishCalc rpc = new ReversePolishCalc();
 
+        // This sets the numbers and operators as string
         String rpnExpression = "2.5,4.8,+";
+        // This is the expected result that is a double
         double expectedResult = 7.3;
+        // rpc is an object and not a class (you would know if it was a cap R and not r)
+        // rpc is a non-staic because it runs on the instance
         double actualResult = rpc.calculate(rpnExpression);
+        // Runs the variables within a method
+        // Checks to see if we're right
         checkResult(rpnExpression, expectedResult, actualResult);
 
         // Write tests for the other operators (-, *, /)
 
         // Uncomment this test which has many operators
-//        rpnExpression = "5,1,2,+,4,*,+,3,-";
-//        expectedResult = 14.0;
-//        actualResult = rpn.calculate(rpnExpression);
-//        checkResult(rpnExpression, expectedResult, actualResult);
+        rpnExpression = "5,1,2,+,4,*,+,3,-";
+        expectedResult = 14.0;
+        actualResult = rpc.calculate(rpnExpression);
+        checkResult(rpnExpression, expectedResult, actualResult);
     }
 
     private static void checkResult(String expression, double expected, double actual) {
--- src/com/theironyard/charlotte/ReversePolishCalc.java
+++ src/com/theironyard/charlotte/ReversePolishCalc.java
@@ -1,5 +1,6 @@
 package com.theironyard.charlotte;
 
+
 public class ReversePolishCalc {
 
     // You'll need a variable here to keep track of the top of the stack
@@ -13,21 +14,28 @@ public class ReversePolishCalc {
 
     public double calculate(String input) {
         // 1. Use the String split method to split the string into tokens at the commas
+        // This will spilt the array into three elements because it will find the comma in between
         tokens = input.split(",");
 
         // 2. Allocate a stack as big as the number of tokens
+        // Creates a "new" constructor
+        // It will take on the new length of an array which is the three elements
         stack = new String[ tokens.length ];
 
         // 3. write the algorithm
+        //
         for(int i = 0; i < tokens.length; ++i) {
             // calls to push() and pop() and do the math here
             String currentToken = tokens[i];
 
             // convert the current token to a number
+            // The big "Double" has methods that is avaialble to use and that's why we use null which is avaialble
             Double num = null;
 
             try {
-             num = Double.parseDouble(currentToken);
+                // "Double.parseDouble" will turn the index string into a number
+                // If it cannot turn it into a number, then it will run the catch block
+                num = Double.parseDouble(currentToken);
             } catch (NumberFormatException ex) {
                 // eat it.
             }
@@ -35,18 +43,33 @@ public class ReversePolishCalc {
             // when we're here, either num has a value
             // or it is null. if it's null, that means
             // we just encountered an operator
+            // If num is not equal to num
+            // If it's a number, it is allowed to be pushed into a new array
+            // It will run the push method which is private void push(double d) instead of private void push (String push)
+            // Why? Because it detects that it's a double and not a string, which is why it will run the first method as stated in
+            // previous line
             if (num != null) {
                 push(num);
+            } else if (currentToken.equals("+")) {
+                double value1 = pop();
+                double value2 = pop();
+                push(value2 + value1);
+            } else if (currentToken.equals(("-"))) {
+                double value1 = pop();
+                double value2 = pop();
+                push(value2 - value1);
+            } else if (currentToken.equals("/")){
+                double value1 = pop();
+                double value2 = pop();
+                push(value2 / value1);
             } else {
-                if (currentToken.equals("+")) {
-                    double value1 = pop();
-                    double value2 = pop();
-
-                    push(value2 + value1);
-                }
-
-                // TODO: implement the rest of the operators
+                double value1 = pop();
+                double value2 = pop();
+                push(value2 * value1);
             }
+
+            // TODO: implement the rest of the operators
+
         }
 
         // 4. return the result
@@ -56,10 +79,15 @@ public class ReversePolishCalc {
     private void push(String number) {
         // push on the stack
         // TODO: write this code that pushes onto the stack
+
+        stack [stackTop] = number;
+        stackTop++;
+
     }
 
     private void push(double d) {
         // change the double to a string and then push it on the stack
+        // in literal sense, it pushes the number into a string push("2.5")
         push(Double.toString(d));
     }
 
